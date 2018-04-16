fff3cab85be489f299cd7ea20f27fd7344f0a3b4
Amanda Wray <amandawray2010@gmail.com>
2018-04-16 10:58:20 -0600

    Add new product migration file & update package.json & add carousel.css

--- migrations/20180415142348_wishlist.js
+++ migrations/20180415142348_wishlist.js
@@ -1,13 +1,13 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('wishlist', function(table){
-    table.increments()
-    table.integer('user_id').notNull().references('users.id').onDelete('CASCADE')
-    // table.____('pid').notNull
-    table.dateTime('created_at').notNullable().defaultTo(knex.raw('now()'))
-    table.dateTime('updated_at').notNullable().defaultTo(knex.raw('now()'))
+    table.increments();
+    table.integer('user_id').notNull().references('users.id').onDelete('CASCADE');
+    table.integer('products_id').notNull().references('products.id').onDelete('CASCADE');
+    table.dateTime('created_at').notNullable().defaultTo(knex.raw('now()'));
+    table.dateTime('updated_at').notNullable().defaultTo(knex.raw('now()'));
   })
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('wishlist')
+  return knex.schema.dropTableIfExists('wishlist');
 };
