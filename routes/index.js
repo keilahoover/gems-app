d1c0f07a3de446bcbfbbad13e57ac3c9956e9757
Amanda Wray <amandawray2010@gmail.com>
2018-04-16 16:29:21 -0600

    Add working seed file

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-
-var craigslist = require('node-craigslist'),
-  client = new craigslist.Client({
-    city : 'boulder'
-  });
-
-client
-  .list()
-  .then((listings) => {
-    // play with listings here...
-    // listings.forEach((listing) => console.log(listing));
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'GEMS' });
--- seeds/001_products.js
+++ seeds/001_products.js
@@ -1,26 +1,110 @@
 
 exports.seed = function(knex, Promise) {
-  const craigslist = require('node-craigslist'),
-    client = new craigslist.Client({
+  const craigslist = require('node-craigslist')
+  const antiqueClient = new craigslist.Client({
       city : 'boulder'
-    });
+  }),
+  options = {
+    baseHost : '', // defaults to craigslist.org
+    category : 'ata', // defaults to sss (all)
+    city : '',
+    maxAsk : '200',
+    minAsk : '100'
+  };
+
+  const bookClient = new craigslist.Client({
+      city : 'boulder'
+    }),
+    bOptions = {
+      baseHost : '', // defaults to craigslist.org
+      category : 'bka', // defaults to sss (all)
+      city : '',
+      maxAsk : '200',
+      minAsk : '100'
+    };
+
+  const gameClient = new craigslist.Client({
+      city : 'boulder'
+    }),
+    gOptions = {
+      baseHost : '', // defaults to craigslist.org
+      category : 'vga', // defaults to sss (all)
+      city : '',
+      maxAsk : '200',
+      minAsk : '100'
+    };
+    const collectClient = new craigslist.Client({
+      city : 'boulder'
+    }),
+    cOptions = {
+      baseHost : '', // defaults to craigslist.org
+      category : 'cba', // defaults to sss (all)
+      city : '',
+      maxAsk : '200',
+      minAsk : '100'
+    };
+
   // Deletes ALL existing entries
   return knex('products').del()
     .then(function () {
-      return client
-        .list()
-        .then((result) => {
-            return knex('products').insert(
-              result.map((listing) => ({
-                pid: listing.pid,
-                hasPic: listing.hasPic,
-                price: listing.price,
-                title: listing.title,
-                url: listing.url
-            })
-          )
-          )
-        })
+      return Promise.all([
+        antiqueClient
+          .search(options, '')
+          .then((result) => {
+              return knex('products').insert(
+                result.map((listing) => ({
+                  pid: listing.pid,
+                  hasPic: listing.hasPic,
+                  price: listing.price,
+                  title: listing.title,
+                  url: listing.url
+              })
+            )
+            )
+          }),
+          bookClient
+            .search(bOptions, '')
+            .then((result) => {
+                return knex('products').insert(
+                  result.map((listing) => ({
+                    pid: listing.pid,
+                    hasPic: listing.hasPic,
+                    price: listing.price,
+                    title: listing.title,
+                    url: listing.url
+                })
+              )
+              )
+            }),
+            gameClient
+              .search(gOptions, '')
+              .then((result) => {
+                  return knex('products').insert(
+                    result.map((listing) => ({
+                      pid: listing.pid,
+                      hasPic: listing.hasPic,
+                      price: listing.price,
+                      title: listing.title,
+                      url: listing.url
+                  })
+                )
+                )
+              }),
+              collectClient
+                .search(cOptions, '')
+                .then((result) => {
+                    return knex('products').insert(
+                      result.map((listing) => ({
+                        pid: listing.pid,
+                        hasPic: listing.hasPic,
+                        price: listing.price,
+                        title: listing.title,
+                        url: listing.url
+                    })
+                  )
+                  )
+                })
+      ])
         .then(function() {
           return knex.raw(`SELECT setval('products_id_seq', (SELECT MAX(id) FROM products));`)
         });
