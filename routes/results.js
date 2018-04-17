ace7bc1afd3ca5a462d57493c1d70c1cbf01788d
ian.burnside89@gmail.com <YOUR EMAIL ADDRESSIan>
2018-04-17 13:13:37 -0600

    add bcrypt and jsonwebtoken dependancies, add basic get and post routes for user sign up

---
+++ routes/results.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const knex = require('../knex');
+const humps = require('humps');
+const KEY = process.env.JWT_KEY
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+
+// /* GET to sign up/login page */
+// router.get('/users', (req, res, next) => {
+//   res.send('this is where users sign up for an account!!');
+// });
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const knex = require('../knex');
+const humps = require('humps');
+const KEY = process.env.JWT_KEY
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  res.send('this is where users sign up for an account');
 });
+
+/*POST adding an account */
+
+router.post('/login' , (req,res,next) => {
+  res.send('created an account!')
+})
 
 module.exports = router;
