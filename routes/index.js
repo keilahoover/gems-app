10fc8cb3954013bcb678e0f1e5bf27c8a2604840
ian.burnside89@gmail.com <YOUR EMAIL ADDRESSIan>
2018-04-17 15:47:36 -0600

    add routes for creating user, logging back in, add .env and dotenv dependancies

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,67 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
 
-/* GET home page. */
+const express = require('express');
+const router = express.Router();
+const knex = require('../knex');
+const humps = require('humps');
+const KEY = process.env.JWT_KEY
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+/* GET to the home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'GEMS' });
+  res.send('welcome to the home page!')
 });
+
+/* login to an account */
+router.post('/', (req, res, next) => {
+  console.log('req.body', req.body);
+  knex('users')
+    .where('email', req.body.email)
+    .then((data) => {
+      if (data.length > 0) {
+        const user = {
+          id: data[0].id,
+          firstName: data[0].first_name,
+          lastName: data[0].last_name,
+          email: data[0].email
+        }
+
+
+
+        bcrypt.compare(req.body.password, data[0].hashed_password)
+          .then((result) => {
+
+
+            if (result === true && req.body.email === data[0].email) {
+
+
+
+              let signedUser = jwt.sign(user, KEY)
+
+              res.cookie('token', signedUser, {
+                  path: '/',
+                  httpOnly: true
+                })
+              
+
+             res.send('youre logged in')
+            } else {
+              // log
+              res.status(400).type('text/plain')
+                .send("Bad email or password")
+
+            }
+
+          })
+          .catch((err) => `>>> CRAAAAAAAPPPPPPPPPPP ${err}`)
+      } else {
+        res.status(400).type('text/plain')
+          .send("Bad email or password")
+      }
+
+    })
+    .catch((err) => `>>> CRAAAAAAAPPPPPPPPPPP ${err}`)
+})
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -16,8 +16,23 @@ router.get('/', (req, res, next) => {
 
 /*POST adding an account */
 
-router.post('/login' , (req,res,next) => {
-  res.send('created an account!')
+router.post('/' , (req,res,next) => {
+  let hashed = bcrypt.hashSync(req.body.password, 8);
+  knex('users')
+  .insert(humps.decamelizeKeys({
+    first_name: req.body.firstName,
+    last_name: req.body.lastName,
+    email: req.body.email,
+    hashed_password: hashed
+  }))
+  .returning(['id', 'first_name', 'last_name', 'email'])
+  .then((data) => {
+    res.json(humps.camelizeKeys(data[0]))
+  })
+  .catch((err) => {
+    next(err)
+  })
+
 })
 
 module.exports = router;
