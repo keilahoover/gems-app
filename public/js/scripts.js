a9d8cc4870126ab750366d78b0caac6c4f05c098
ian.burnside89@gmail.com <YOUR EMAIL ADDRESSIan>
2018-04-18 16:27:34 -0600

    wishlist button ready for routing

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -10,14 +10,12 @@
 $(document).ready(function() {
   console.log('ready to go ');
 
+  $('#user-wishlist').click(function() {
+    event.preventDefault()
+    console.log("you clicked me bro");
+  })
 
-  // $('#log-in').click(function() {
-  //   event.preventDefault()
-  //
-  //     console.log('you clicked on login button');
-  //
-  //
-  // })
+
 
 
   $('#sign-up').click(function() {
