9b2ba1173c6a568a9791153dd2090e5c29ddc084
ian.burnside89@gmail.com <YOUR EMAIL ADDRESSIan>
2018-04-18 12:18:40 -0600

    add event listeners in scripts.js

---
+++ public/js/scripts.js
@@ -0,0 +1,6 @@
+// create event listener for register/login buttomn
+//when register is clicked, reroute to sign up page
+//after input in added, reroute to index.ejs 
+
+//create event listener for log in button
+// when clicked , user is rerouted to wishlist
