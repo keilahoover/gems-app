8a4ae4c3cfb70c296b701eb2f0c4e1891e200504
ian.burnside89@gmail.com <YOUR EMAIL ADDRESSIan>
2018-04-19 11:23:57 -0600

    things kind of working

--- knexfile.js
+++ knexfile.js
@@ -4,20 +4,15 @@ module.exports = {
 
   development: {
     client: 'pg',
-<<<<<<< HEAD
+
     connection: 'postgres://localhost/gems_app_dev'
-=======
-    connection: 'postgres:localhost/gems_app_dev'
->>>>>>> db61035c50ccbe68dc2f325ac079a690976d340c
+
   },
 
     test: {
       client: 'pg',
-<<<<<<< HEAD
+
       connection: 'postgres://localhost/gems_app_test'
-=======
-      connection: 'postgres:localhost:gems_app_test'
->>>>>>> db61035c50ccbe68dc2f325ac079a690976d340c
   },
 
   production: {
