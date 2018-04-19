cfa511461ff1004bf4ab75d61977e0ece4746ed4
Keila Hoover <keila.hoover@gmail.com>
2018-04-19 12:08:20 -0600

    Update About Us page and collectible carousel image

--- routes/index.js
+++ routes/index.js
@@ -13,47 +13,48 @@ const bcrypt = require('bcrypt');
 
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'GEMS' });
- })
+ res.render('index', { title: 'GEMS' });
+})
+
 
 /* login to an account */
 router.post('/', (req, res, next) => {
-  //console.log('req.body', req.body);
-  knex('users')
-    .where('email', req.body.email)
-    .then((data) => {
-      if (data.length > 0) {
-        const user = {
-          id: data[0].id,
-          firstName: data[0].first_name,
-          lastName: data[0].last_name,
-          email: data[0].email
-        }
-        bcrypt.compare(req.body.password, data[0].hashed_password)
-        .then((result) => {
-          if (result === true && req.body.email === data[0].email) {
-            let signedUser = jwt.sign(user, KEY)
+ //console.log('req.body', req.body);
+ knex('users')
+   .where('email', req.body.email)
+   .then((data) => {
+     if (data.length > 0) {
+       const user = {
+         id: data[0].id,
+         firstName: data[0].first_name,
+         lastName: data[0].last_name,
+         email: data[0].email
+       }
+       bcrypt.compare(req.body.password, data[0].hashed_password)
+       .then((result) => {
+         if (result === true && req.body.email === data[0].email) {
+           let signedUser = jwt.sign(user, KEY)
 
-            res.cookie('token', signedUser, {
-              path: '/',
-              httpOnly: true
-            })
+           res.cookie('token', signedUser, {
+             path: '/',
+             httpOnly: true
+           })
 
-            res.render('index', { title: 'GEMS', signinSuccess: true });
-          } else {
-            // log
-            res.status(400).type('text/plain')
-            .send("Bad email or password")
-          }
+           res.render('index', { title: 'GEMS', signinSuccess: true });
+         } else {
+           // log
+           res.status(400).type('text/plain')
+           .send("Bad email or password")
+         }
 
-        })
-        .catch((err) => `>>> CRAAAAAAAPPPPPPPPPPP ${err}`)
-      } else {
-        res.status(400).type('text/plain')
-        .send("Bad email or password")
-      }
+       })
+       .catch((err) => `>>> CRAAAAAAAPPPPPPPPPPP ${err}`)
+     } else {
+       res.status(400).type('text/plain')
+       .send("Bad email or password")
+     }
 
-    })
-    .catch((err) => `>>> CRAAAAAAAPPPPPPPPPPP ${err}`)
-  })
-  module.exports = router;
+   })
+   .catch((err) => `>>> CRAAAAAAAPPPPPPPPPPP ${err}`)
+ })
+ module.exports = router;
